947ffb719cd81147904da220aa47552f9bae5c06
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-11 10:15:59 +0100

    direct:calculate-vmworkloadinventory

--- src/main/java/org/jboss/xavier/integrations/route/VMWorkloadInventoryRoutes.java
+++ src/main/java/org/jboss/xavier/integrations/route/VMWorkloadInventoryRoutes.java
@@ -25,15 +25,7 @@ public class VMWorkloadInventoryRoutes extends RouteBuilderExceptionHandler {
             .setHeader("KieSessionId", constant("WorkloadInventoryKSession0"))
             .bean("VMWorkloadInventoryCalculator", "calculate(${body}, ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}})", false)
                 .process(exchange -> {
-                    Set<String> vmNamesWithSharedDisk = exchange.getIn().getHeader("vmNamesWithSharedDisk", Set.class);
                     Collection<VMWorkloadInventoryModel>  allVms = exchange.getIn().getBody(Collection.class);
-                    Collection<VMWorkloadInventoryModel> originalAndAmendedVms = allVms.stream().map(vmWorkloadInventoryModel ->
-                    {
-                        if (vmNamesWithSharedDisk != null && vmNamesWithSharedDisk.contains(vmWorkloadInventoryModel.getVmName())) {
-                            vmWorkloadInventoryModel.setHasSharedVmdk(true);
-                        }
-                        return vmWorkloadInventoryModel;
-                    }).collect(Collectors.toList());
                     exchange.getIn().setBody(allVms);
                     exchange.getIn().removeHeader("vmNamesWithSharedDisk");
                     exchange.getIn().removeHeader("originalBody");
@@ -50,5 +42,20 @@ public class VMWorkloadInventoryRoutes extends RouteBuilderExceptionHandler {
             .transform().method("decisionServerHelper", "generateCommands(${body}, \"GetWorkloadInventoryReports\", ${header.KieSessionId})")
             .to("direct:decisionserver").id("workload-decisionserver")
             .transform().method("decisionServerHelper", "extractWorkloadInventoryReportModel");
+
+        from("direct:flags-shared-disks").routeId("flags-shared-disks")
+                .process(exchange -> {
+                    String originalBody = exchange.getIn().getBody(String.class);
+                    exchange.getIn().setHeader("originalBody", originalBody);
+                })
+            .bean("flagSharedDisksCalculator", "calculate(${body}, ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}})", false)
+            .process(exchange -> {
+                String originalBody = exchange.getIn().getHeader("originalBody", String.class);
+                Set<String> vmNamesWithSharedDisk = exchange.getIn().getBody(Set.class);
+                exchange.getIn().setHeader("vmNamesWithSharedDisk", vmNamesWithSharedDisk);
+                exchange.getIn().setBody(originalBody);
+            }).to("direct:calculate-vmworkloadinventory");
+
+
     }
 }
