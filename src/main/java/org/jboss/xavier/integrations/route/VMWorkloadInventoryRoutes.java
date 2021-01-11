3d386329c1c40c04441b4d8cdddfe2f9382fc95d
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-11 10:06:28 +0100

    direct:calculate-vmworkloadinventory

--- src/main/java/org/jboss/xavier/integrations/route/VMWorkloadInventoryRoutes.java
+++ src/main/java/org/jboss/xavier/integrations/route/VMWorkloadInventoryRoutes.java
@@ -1,11 +1,15 @@
 package org.jboss.xavier.integrations.route;
 
+import org.jboss.xavier.analytics.pojo.input.workload.inventory.VMWorkloadInventoryModel;
 import org.jboss.xavier.integrations.route.strategy.WorkloadInventoryReportModelAggregationStrategy;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.inject.Named;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Named
 public class VMWorkloadInventoryRoutes extends RouteBuilderExceptionHandler {
@@ -20,8 +24,23 @@ public class VMWorkloadInventoryRoutes extends RouteBuilderExceptionHandler {
         from("direct:calculate-vmworkloadinventory").routeId("calculate-vmworkloadinventory")
             .setHeader("KieSessionId", constant("WorkloadInventoryKSession0"))
             .bean("VMWorkloadInventoryCalculator", "calculate(${body}, ${header.${type:org.jboss.xavier.integrations.route.MainRouteBuilder.MA_METADATA}})", false)
-            .split(body()).parallelProcessing(parallel).aggregationStrategy(new WorkloadInventoryReportModelAggregationStrategy())
+                .process(exchange -> {
+                    Set<String> vmNamesWithSharedDisk = exchange.getIn().getHeader("vmNamesWithSharedDisk", Set.class);
+                    Collection<VMWorkloadInventoryModel>  allVms = exchange.getIn().getBody(Collection.class);
+                    Collection<VMWorkloadInventoryModel> originalAndAmendedVms = allVms.stream().map(vmWorkloadInventoryModel ->
+                    {
+                        if (vmNamesWithSharedDisk != null && vmNamesWithSharedDisk.contains(vmWorkloadInventoryModel.getVmName())) {
+                            vmWorkloadInventoryModel.setHasSharedVmdk(true);
+                        }
+                        return vmWorkloadInventoryModel;
+                    }).collect(Collectors.toList());
+                    exchange.getIn().setBody(allVms);
+                    exchange.getIn().removeHeader("vmNamesWithSharedDisk");
+                    exchange.getIn().removeHeader("originalBody");
+                })
+                .split(body()).parallelProcessing(parallel).aggregationStrategy(new WorkloadInventoryReportModelAggregationStrategy())
                 .setHeader(ANALYSIS_ID, simple("${body." + ANALYSIS_ID + "}", String.class))
+                .removeHeader("vmNamesWithSharedDisk")
                 .to("direct:vm-workload-inventory")
             .end()
             .process(exchange -> analysisService.addWorkloadInventoryReportModels(exchange.getIn().getBody(List.class),
